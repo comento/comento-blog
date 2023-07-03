5551bd9d99e33da2cbb67c43497120c6026e961a
LuiGeeDev <yjs3475@gmail.com>
2023-07-03 16:39:41 +0900

    fix: ghost 경로 변경, Footer 임시 삭제

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,7 +1,7 @@
 import GhostContentAPI from "@tryghost/content-api";
 
 export const ghostClient = new GhostContentAPI({
-  url: "http://52.79.239.39:2370",
+  url: "http://blog.comento.info:2370",
   key: import.meta.env.CONTENT_API_KEY,
   version: "v5.0",
 });
