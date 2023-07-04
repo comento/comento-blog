5f903b93b68467780f0e1472b88bd7e7177fbb94
LuiGeeDev <yjs3475@gmail.com>
2023-07-04 16:42:39 +0900

    feat: 쿼리 최적화

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
   readonly CONTENT_API_KEY: string;
+  readonly ADMIN_API_KEY: string;
 }
--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,7 +1,14 @@
 import GhostContentAPI from "@tryghost/content-api";
+import GhostAdminAPI from "@tryghost/admin-api";
 
 export const ghostClient = new GhostContentAPI({
   url: "http://blog.comento.info:2370",
   key: import.meta.env.CONTENT_API_KEY,
+  version: "v5.0",
+});
+
+export const adminClient = new GhostAdminAPI({
+  url: "http://blog.comento.info:2370",
+  key: import.meta.env.ADMIN_API_KEY,
   version: "v5.0",
 });
