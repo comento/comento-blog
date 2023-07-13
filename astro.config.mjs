05783f54cdd987edeb90c464f4d3f2affd397763
LuiGeeDev <yjs3475@gmail.com>
2023-07-13 11:01:44 +0900

    feat: canonical URL 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://developer.comento.kr",
   integrations: [tailwind(), sitemap()],
+  trailingSlash: "never",
 });
