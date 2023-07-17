70fa4f5e014b8501cc25aac70f76106cc25909dc
LuiGeeDev <yjs3475@gmail.com>
2023-07-17 14:51:35 +0900

    fix: 콘솔 에러 수정

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 # root 계정으로 새로운 계정을 생성하고 그 계정으로 스크립트를 실행해주세요
-# adduser comento
-# usermod -aG sudo comento
-# su - comento
+# adduser <username>
+# usermod -aG sudo <username>
+# su - <username>
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -14,7 +14,7 @@ curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash
 sudo apt-get install -y nodejs
 sudo npm install ghost-cli@latest -g
 sudo mkdir -p /var/www/blog
-sudo chown comento:comento /var/www/blog
+sudo chown <username>:<username> /var/www/blog
 sudo chmod 775 /var/www/blog
 cd /var/www/blog
 ghost install
