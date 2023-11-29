96825a8cb1a78f14c787bcfbd4a3d4d7975b1b97
LuiGeeDev <yjs3475@gmail.com>
2023-11-29 15:58:03 +0900

    feat: Partytown 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://developer.comento.kr",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   trailingSlash: "never",
 });
