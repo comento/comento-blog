207baeddc540619d9ca2f7bcfeac2f80685a71dd
LuiGeeDev <yjs3475@gmail.com>
2023-12-26 15:05:08 +0900

    feat: 목차 컴포넌트

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://developer.comento.kr",
-  integrations: [
-    tailwind(),
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
-  trailingSlash: "never",
+  integrations: [tailwind(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), vue()],
+  trailingSlash: "never"
 });
---
+++ src/components/TableOfContents.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { computed, onMounted, ref } from "vue";
+
+type Heading = {
+  id: string;
+  level: number;
+  title: string;
+  children?: Heading[];
+};
+
+const props = defineProps<{
+  headings: Heading[];
+}>();
+
+const headingsByLevel = computed(() => {
+  if (!props.headings.length) {
+    return [];
+  }
+
+  const topLevel: Heading[] = [];
+  const current: {
+    h2?: Heading;
+    h3?: Heading;
+  } = {};
+
+  props.headings.reduce((prev, curr) => {
+    curr.children = [];
+
+    if (curr.level === 2) {
+      topLevel.push(curr);
+      current.h2 = curr;
+    } else if (curr.level === 3) {
+      current.h2.children.push(curr);
+      current.h3 = curr;
+    }
+
+    return prev;
+  }, {});
+
+  return topLevel;
+});
+
+const activeHeading = ref({
+  level2: headingsByLevel.value[0].id,
+  level3: null,
+});
+
+onMounted(() => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      const entry = entries.find((entry) => entry.isIntersecting);
+      if (entry) {
+        entry.target.tagName === "H2"
+          ? (activeHeading.value.level2 = entry.target.id)
+          : (activeHeading.value.level3 = entry.target.id);
+      }
+    },
+    {
+      rootMargin: "0px 0px -50% 0px",
+    }
+  );
+
+  document.querySelectorAll("h2, h3").forEach((heading) => {
+    observer.observe(heading);
+  });
+});
+</script>
+
+<template>
+  <ol>
+    <li
+      v-for="{ id, title, children } in headingsByLevel"
+      :key="id"
+      class="text-gray-400 mb-2.5"
+    >
+      <a :href="`#${id}`" :class="{ active: activeHeading.level2 === id }">
+        {{ title }}
+      </a>
+      <ol v-show="activeHeading.level2 === id">
+        <li v-for="child in children">
+          <a
+            :href="`#${child.id}`"
+            class="text-sm ml-5"
+            :class="{ active: activeHeading.level3 === child.id }"
+          >
+            {{ child.title }}
+          </a>
+        </li>
+      </ol>
+    </li>
+  </ol>
+</template>
+
+<style scoped>
+a:hover,
+.active {
+  @apply text-gray-900;
+}
+</style>
